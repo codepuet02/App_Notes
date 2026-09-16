8dcf5c9a83870e71aafd9aaf9e01de5cdf55037f
Davison Andres <puentesdavison@gmail.com>
2026-09-16 10:54:15 -0500

    primer commit:estructura base de el proyecto

---
+++ src/main/java/com/andres/app_Notes/AppNotesApplication.java
@@ -0,0 +1,13 @@
+package com.andres.app_Notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppNotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andres/app_Notes/AppNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andres.app_Notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
